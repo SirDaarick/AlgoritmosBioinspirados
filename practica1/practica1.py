6d2604c86f9619a73e94e2ec3d4e4a2809748330
Daarick <e.danielgrz10@gmail.com>
2025-03-11 11:02:51 -0600

    avance

--- practica1/practica1.py
+++ practica1/practica1.py
@@ -6,8 +6,13 @@ global TAM_POBLACION
 global N_ALELOS
 global N 
 TAM_POBLACION = 400
-N = 10
+N = 10  
 N_ALELOS = N**2
+#usar la siguiente lista de numeros primos para la seed
+#2, 3, 5, 7, 11, 13, 17, 19
+seed = 13
+random.seed(seed)
+np.random.seed(seed)
 
 class Poblacion:
     def __init__(self):
@@ -64,7 +69,7 @@ def algoritmoGenetico(TAM_POBLACION, pCruza, pMuta, max_generaciones=1000):
 
         for i in range(0, len(padres), 2):
             if i+1 < len(padres) and random.random() <= pCruza:
-                hijos.extend(cruzaNpuntos([padres[i], padres[i+1]], 0.5))
+                hijos.extend(cruzaNpuntos([padres[i], padres[i+1]], 0.2))
 
         for hijo in hijos:
             if random.random() <= pMuta:
@@ -89,7 +94,6 @@ def algoritmoGenetico(TAM_POBLACION, pCruza, pMuta, max_generaciones=1000):
         #print("Promedio de aptitud de la población: ", poblacion.promedioAptitud(), "\n")
 
         if poblacion.mejorIndividuo().aptitud == 0:
-            print(poblacion.mejorIndividuo())
             break
 
         if poblacion.mejorIndividuo() == poblacion.peorIndividuo(): #Criterio de paro 2
@@ -97,7 +101,11 @@ def algoritmoGenetico(TAM_POBLACION, pCruza, pMuta, max_generaciones=1000):
             break
     
     graficar(mejor, peor, promedio)
+    print(f"Mejor individuo {poblacion.mejorIndividuo().cromosoma}")
     print("Finalizó el algoritmo")
+    
+    matriz = cromosoma_a_matriz(poblacion.mejorIndividuo().cromosoma, N)
+    graficar_nonograma(matriz)
 
 def graficar(mejores, peores, promedios):
     generaciones = list(range(1, len(mejores) + 1))
@@ -114,6 +122,19 @@ def graficar(mejores, peores, promedios):
     plt.grid(True)  
 
     plt.show()
+    
+    
+def graficar_nonograma(matriz):
+    n = len(matriz)
+    plt.figure(figsize=(n, n))
+    plt.imshow(matriz, cmap='binary', vmin=0, vmax=1)  # Usar colores binarios (blanco y negro)
+    plt.xticks(range(n))  # Mostrar líneas de la cuadrícula
+    plt.yticks(range(n))
+    plt.grid(color='black', linewidth=1)  # Cuadrícula negra
+    plt.show()
+    
+def cromosoma_a_matriz(cromosoma, n):
+    return np.array(cromosoma).reshape(n, n).tolist()
 
 def generaPoblacion(tam_pob):
     return [Individuo() for _ in range(tam_pob)]
@@ -143,6 +164,7 @@ def aptitud(individuo):
     #reglas_c = [(0,0,3), (0,2,2), (0,0,4), (0,2,2), (0,0,3)]
     
     #10X10 mejores parametros 0.5, 0.3 con 400 de poblacion
+    
     reglas_r = [
         [0, 0, 0, 0, 10],
         [0, 0, 0, 3, 3],
@@ -160,15 +182,15 @@ def aptitud(individuo):
         [0, 0, 0, 0, 10],
         [0, 0, 0, 3, 3],
         [0, 0, 2, 1, 2],
-        [0, 1, 2, 1, 1], #quizas es 1121
+        [0, 1, 1, 2, 1], #quizas es 1121
         [0, 0, 1, 2, 1],
         [0, 0, 1, 2, 1],
-        [0, 1, 2, 1, 1],
+        [0, 1, 1, 2, 1],
         [0, 0, 2, 1, 2],
         [0, 0, 0, 3, 3],
         [0, 0, 0, 0, 10]
     ]
-
+    
     return nonograma(individuo.cromosoma, reglas_r, reglas_c, N) 
 
 def cruzaUnPunto(pareja):
@@ -284,4 +306,5 @@ def decodificar(cromosoma, n):
     return reglas
 
 
-algoritmoGenetico(TAM_POBLACION, 0.6, 0.3, 100000)
+algoritmoGenetico(TAM_POBLACION, 0.5, 0.3, 100000)
+
